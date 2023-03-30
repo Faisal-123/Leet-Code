20d45851d13c13dc608aa5b7e325ada8643b9377
Faisal-123 <129355869+Faisal-123@users.noreply.github.com>
2023-03-30 12:50:31 +0500

    Create Capacity_To_Ship_Packages_Within_D_Days.cpp

---
+++ Binary_Search/Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -0,0 +1,23 @@
+    int check(vector<int>& w,int m)
+    {
+        int cur=0,cnt=0;
+        for(auto i:w)
+        {
+            if(cur+i>m)
+            {
+                cnt++;
+            }
+        }
+    }
+    int shipWithinDays(vector<int>& w, int d) {
+        int s=0,e=0;
+        for(auto i:w)
+        {
+            e+=i;
+        }
+        e+=10;
+        while(s+1<e)
+        {
+
+        }
+    }
