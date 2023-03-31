4b1427df47c8141b64e81b5e6a59b1e8fd92d22d
Faisal-123 <129355869+Faisal-123@users.noreply.github.com>
2023-03-31 06:50:36 +0500

    Update Capacity_To_Ship_Packages_Within_D_Days.cpp

--- Binary_Search/Capacity_To_Ship_Packages_Within_D_Days.cpp
+++ Binary_Search/Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -10,18 +10,31 @@
             }
             cur+=i;
         }
-        cnt++;
+        cnt+=(cur>0);
         return cnt;
     }
     int shipWithinDays(vector<int>& w, int d) {
         int s=0,e=0;
         for(auto i:w)
         {
+            s=max(s,i);
             e+=i;
         }
         e+=10;
         while(s+1<e)
         {
-
+            int mid=(s+e)/2;
+            if(check(w,mid)<=d)
+            {
+                e=mid;
+            }
+            else{
+                s=mid;
+            }
         }
+        if(check(w,s)<=d)
+        {
+            e=s;
+        }
+        return e;
     }
